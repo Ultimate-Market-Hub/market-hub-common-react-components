59e564320a95fdda5c054363819e2bebb7b5ba79
Hill Matthew <matthill8286@gmail.com>
2023-12-21 15:18:14 +0200

    fix: remove link with button

--- src/components/atoms/LinkButton.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from 'react-router-dom';
-
-import { Button, type ButtonProps } from '~/components/atoms/Button';
-
-type LinkButtonProps = Omit<ButtonProps<typeof Link>, 'as'>;
-
-export const LinkButton = (props: LinkButtonProps) => (
-  <Button {...props} as={Link} />
-);
